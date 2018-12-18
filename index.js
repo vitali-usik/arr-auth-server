bc9b7602df9833ae95047dc74eb9349af601e50e
vitali-usik <usik.vitaly@gmail.com>
2018-12-18 17:51:44 +0300

    add cors lib and permit request from any domain

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const http = require('http');
 const bodyParser = require('body-parser');
 const morgan = require('morgan');
 const mongoose = require('mongoose');
+const cors = require('cors');
 
 const router = require('./router');
 
@@ -13,6 +14,7 @@ mongoose.connect('mongodb://localhost:27017/auth');
 
 // App Setup
 app.use(morgan('combined'));
+app.use(cors());
 app.use(bodyParser.json({ type: '*/*' }));
 router(app);
 
